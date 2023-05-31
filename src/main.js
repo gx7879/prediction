2d9c0ca5e9b181ef07da011f33606a71249c2290
576 <double@double-cash.com>
2023-05-31 23:52:38 +0800

    slider完成

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-import './index.css'
+import "./index.css";
+// import Swiper JS
+import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
